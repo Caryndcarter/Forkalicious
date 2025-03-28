53a30dad8ca434844e3736162519fe71d8bea538
Caryn Carter <carter.caryn@gmail.com>
2025-03-28 16:37:34 -0500

    fixed cdk-stack lamda dist location for the code

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -101,8 +101,8 @@ export class CdkStack extends cdk.Stack {
   // 9 .Create Lambda function for your backend
   const backendFunction = new lambda.Function(this, 'BackendFunction', {
     runtime: lambda.Runtime.NODEJS_20_X,
-    handler:  'dist/lambda.handler',
-    code: lambda.Code.fromAsset('../../server'),
+    handler:  'lambda.handler',
+    code: lambda.Code.fromAsset('../../server/dist'),
     environment: {
       JWT_SECRET_KEY: ssm.StringParameter.valueForStringParameter(this, '/forkalicious/jwt-secret'),
       SPOONACULAR_API_KEY: ssm.StringParameter.valueForStringParameter(this, '/forkalicious/spoonacular-api-key'),
