01ff0b33428ad59d8a53a685a9bae743545286fe
Caryn Carter <carter.caryn@gmail.com>
2025-04-18 17:30:02 -0500

    added CDK bucket check to see if it exists before creating a new one

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -15,14 +15,6 @@ import * as path from 'path';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as logs from 'aws-cdk-lib/aws-logs';
 
-
-
-import {
-  CloudFrontWebDistribution,
-  ViewerCertificate,
-  SSLMethod,
-  SecurityPolicyProtocol,
-} from "aws-cdk-lib/aws-cloudfront";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 interface CdkStackProps extends cdk.StackProps {
@@ -33,9 +25,8 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: CdkStackProps) {
     super(scope, id, props);
 
-  
-     // Add a check for props
-     if (!props?.envName) {
+    // Add a check for props
+    if (!props?.envName) {
       throw new Error('Environment name is required');
     }
 
@@ -44,19 +35,74 @@ export class CdkStack extends cdk.Stack {
       ? 'dev.forkalicious.isawesome.xyz'
       : 'forkalicious.isawesome.xyz';
   
-    
-    // The code that defines your stack goes here
-    // 1. S3 Bucket
-    const destinationBucket = new s3.Bucket(this, `${props.envName}DestinationBucket`, {
-      accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
-      //autoDeleteObjects: true,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
-      publicReadAccess: true,
-      removalPolicy: cdk.RemovalPolicy.RETAIN,
-      // versioned: false,
-      websiteErrorDocument: "index.html",
-      websiteIndexDocument: "index.html",
+    // 1. S3 Bucket - Check if bucket exists, if not create it
+    const bucketName = props.envName === 'dev'
+      ? 'devcdkstack-devdestinationbucket69d16b35-v9x1dxx2c4gn'
+      : 'prodcdkstack-proddestinationbucket69d16b35-xxxxx';
+
+    // Create a custom resource to check if bucket exists
+    const checkBucketFunction = new lambda.Function(this, 'CheckBucketFunction', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        const AWS = require('aws-sdk');
+        const s3 = new AWS.S3();
+        
+        exports.handler = async (event) => {
+          const bucketName = event.ResourceProperties.bucketName;
+          
+          try {
+            await s3.headBucket({ Bucket: bucketName }).promise();
+            return { bucketExists: true };
+          } catch (error) {
+            if (error.code === 'NotFound') {
+              return { bucketExists: false };
+            }
+            throw error;
+          }
+        };
+      `),
     });
+
+    checkBucketFunction.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ['s3:HeadBucket'],
+        resources: ['*'],
+      })
+    );
+
+    const checkBucket = new cdk.CustomResource(this, 'CheckBucket', {
+      serviceToken: new cdk.CustomResourceProvider(this, 'CheckBucketProvider', {
+        runtime: cdk.CustomResourceProviderRuntime.NODEJS_20_X,
+        codeDirectory: path.join(__dirname, 'custom-resource'),
+        handler: 'index.handler',
+      }).serviceToken,
+      properties: {
+        bucketName: bucketName,
+      },
+    });
+
+    let destinationBucket: s3.IBucket;
+
+    // If bucket doesn't exist, create it
+    if (!checkBucket.getAttString('bucketExists')) {
+      destinationBucket = new s3.Bucket(this, `${props.envName}DestinationBucket`, {
+        bucketName: bucketName,
+        accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+        publicReadAccess: true,
+        removalPolicy: cdk.RemovalPolicy.RETAIN,
+        websiteErrorDocument: "index.html",
+        websiteIndexDocument: "index.html",
+      });
+    } else {
+      // Use existing bucket
+      destinationBucket = s3.Bucket.fromBucketName(
+        this,
+        `${props.envName}ExistingBucket`,
+        bucketName
+      );
+    }
 
     // Export the bucket name for use in GitHub Actions
     new cdk.CfnOutput(this, 'BucketName', {
@@ -76,26 +122,24 @@ export class CdkStack extends cdk.Stack {
       });
     }
 
-
-     // 3. ACM Certificate (must be in us-east-1 for CloudFront)
+    // 3. ACM Certificate (must be in us-east-1 for CloudFront)
     const certificate = new acm.Certificate(this, `${props.envName}Certificate`, {
       domainName: domainName,
       validation: acm.CertificateValidation.fromDns(hostedZone),
     });
 
-
     // 4. Create the OAI
-  const oai = new OriginAccessIdentity(this, "WebsiteOAI", {
-    comment: "OAI for website content"
-  });
+    const oai = new OriginAccessIdentity(this, "WebsiteOAI", {
+      comment: "OAI for website content"
+    });
 
-  new cdk.CfnOutput(this, 'OAIId', {
-    value: oai.originAccessIdentityId,
-    description: 'OAI ID for CloudFront'
-  });
+    new cdk.CfnOutput(this, 'OAIId', {
+      value: oai.originAccessIdentityId,
+      description: 'OAI ID for CloudFront'
+    });
 
     // 5. Add bucket policy
-   destinationBucket.addToResourcePolicy(
+    destinationBucket.addToResourcePolicy(
       new PolicyStatement({
         actions: ["s3:GetObject"],
         resources: [`${destinationBucket.bucketArn}/*`],
@@ -103,8 +147,8 @@ export class CdkStack extends cdk.Stack {
       })
     );
 
-// 6. Create CloudFront distribution
-   const distribution = new cloudfront.Distribution(this, `${props.envName}CloudFrontDist`, {
+    // 6. Create CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, `${props.envName}CloudFrontDist`, {
       defaultBehavior: {
         cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
         origin: new origins.S3Origin(destinationBucket, { originAccessIdentity: oai }),
@@ -114,18 +158,17 @@ export class CdkStack extends cdk.Stack {
       certificate,
     });
 
- // 7. Create Route53 record
-      new route53.ARecord(this, `${props.envName}CloudFrontAliasRecord`, {
-        zone: hostedZone,
-        recordName: props.envName === 'dev' ? 'dev' : '', // 'dev' subdomain for dev, root for prod
-        target: route53.RecordTarget.fromAlias(new route53_targets.CloudFrontTarget(distribution)),
-      });
+    // 7. Create Route53 record
+    new route53.ARecord(this, `${props.envName}CloudFrontAliasRecord`, {
+      zone: hostedZone,
+      recordName: props.envName === 'dev' ? 'dev' : '', // 'dev' subdomain for dev, root for prod
+      target: route53.RecordTarget.fromAlias(new route53_targets.CloudFrontTarget(distribution)),
+    });
 
-  console.log('Current working directory:', process.cwd());
-  console.log('Asset path being used:', path.join(__dirname, '../../client/dist'));
+    console.log('Current working directory:', process.cwd());
+    console.log('Asset path being used:', path.join(__dirname, '../../client/dist'));
 
-
-// 8. Deploy the frontend assets to S3
+    // 8. Deploy the frontend assets to S3
     new s3deploy.BucketDeployment(this, 'clientDeploy', {
       sources: [s3deploy.Source.asset(path.resolve(__dirname, '../../client/dist'))],
       destinationBucket: destinationBucket,
@@ -142,139 +185,107 @@ export class CdkStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY // or RETAIN if you want to keep logs after stack deletion
     });
 
-
-  // 9 .Create Lambda function for your backend
-  const backendFunction = new lambda.Function(this, 'BackendFunction', {
-    functionName: `${props.envName}-forkalicious-backend`,
-    runtime: lambda.Runtime.NODEJS_20_X,
-    handler: 'lambda.handler',
-    code: lambda.Code.fromAsset(path.resolve(__dirname, '../../server/dist')),
-    environment: {
-      JWT_SECRET_KEY: ssm.StringParameter.valueForStringParameter(this, '/forkalicious/jwt-secret'),
-      SPOONACULAR_API_KEY: ssm.StringParameter.valueForStringParameter(this, '/forkalicious/spoonacular-api-key'),
-      API_BASE_URL: 'https://api.spoonacular.com',
-      OPENAI_API_KEY: ssm.StringParameter.valueForStringParameter(this, '/forkalicious/openai-api-key'),
-      MONGODB_URI: ssm.StringParameter.valueForStringParameter(this, '/forkalicious/mongodb-uri'),
-      NODE_ENV: props.envName === 'prod' ? 'production' : 'development'
-    },
-    timeout: cdk.Duration.seconds(30),
-    memorySize: 256,
-    logRetention: logs.RetentionDays.ONE_WEEK
-  });
-
-  // Add permissions for SSM, CloudWatch, and basic execution
-  backendFunction.addToRolePolicy(new iam.PolicyStatement({
-    effect: iam.Effect.ALLOW,
-    actions: [
-      'ssm:GetParameter',
-      'ssm:GetParameters',
-      'ssm:GetParametersByPath',
-      'logs:CreateLogGroup',
-      'logs:CreateLogStream',
-      'logs:PutLogEvents'
-    ],
-    resources: [
-      `arn:aws:ssm:${this.region}:${this.account}:parameter/forkalicious/*`,
-      `arn:aws:logs:${this.region}:${this.account}:*`
-    ]
-  }));
-
-  // Create API Gateway logging role
-  const apiGatewayLoggingRole = new iam.Role(this, 'ApiGatewayLoggingRole', {
-    assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com'),
-    managedPolicies: [
-      iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonAPIGatewayPushToCloudWatchLogs')
-    ]
-  });
-
-  // Create API Gateway account settings
-  const apiGatewayAccount = new apigateway.CfnAccount(this, 'ApiGatewayAccount', {
-    cloudWatchRoleArn: apiGatewayLoggingRole.roleArn
-  });
-
-  // Create API Gateway
-  const api = new apigateway.RestApi(this, `${props.envName}BackendApi`, {
-    restApiName: `${props.envName}BackendService`,
-    deploy: true,
-    deployOptions: {
-      stageName: 'prod',  // This is required by API Gateway
-      loggingLevel: apigateway.MethodLoggingLevel.INFO,
-      dataTraceEnabled: true
-    },
-    defaultCorsPreflightOptions: {
-      allowOrigins: apigateway.Cors.ALL_ORIGINS,
-      allowMethods: apigateway.Cors.ALL_METHODS,
-      allowHeaders: [
-        'Content-Type',
-        'X-Amz-Date',
-        'Authorization',
-        'X-Api-Key',
-        'X-Amz-Security-Token',
-        'X-Amz-User-Agent',
-        'Apollo-Require-Preflight',
-        'x-apollo-operation-name',
-        'x-apollo-operation-type'
-      ],
-      allowCredentials: true
-    }
-  });
-
-  // Add dependency to ensure account settings are created first
-  api.node.addDependency(apiGatewayAccount);
-
-  // Make sure the integration is properly set up
-  const backendIntegration = new apigateway.LambdaIntegration(backendFunction, {
-    proxy: true
-  });
-
-  // Add a test endpoint to verify basic connectivity
-  const testResource = api.root.addResource('test');
-  testResource.addMethod('GET', backendIntegration);
-
-  // Add GraphQL endpoint
-  const graphqlResource = api.root.addResource('graphql');
-  graphqlResource.addMethod('POST', backendIntegration);
-
-  // Add proxy integration for all other routes
-  api.root.addProxy({
-    defaultIntegration: backendIntegration,
-    anyMethod: true
-  });
-
-  new cdk.CfnOutput(this, 'ApiGatewayUrl', {
-    value: api.url,
-    description: 'API Gateway URL'
-  });
-    
-    // const apiGatewayAccount = new apigateway.CfnAccount(this, 'ApiGatewayAccount', {
-    //   cloudWatchRoleArn: apiGatewayLoggingRole.roleArn
-    // });
-
-    // const apiGatewayDeployment = api.node.findChild('Deployment') as apigateway.CfnDeployment;
-    // apiGatewayDeployment.addDependsOn(apiGatewayAccount);   
-
-    // 11. Connect API Gateway to Lambda
-    // const backendIntegration = new apigateway.LambdaIntegration(backendFunction);
-    // api.root.addProxy({
-    //   defaultIntegration: backendIntegration,
-    //   anyMethod: true 
-    // });
- 
-    /*
-    // 4. CloudFront Origin Access Control (OAC)
-    const oac = new cloudfront.CfnOriginAccessControl(this, "OAC", {
-      originAccessControlConfig: {
-        name: "S3OAC",
-        originAccessControlOriginType: "s3",
-        signingBehavior: "always",
-        signingProtocol: "sigv4",
+    // 9 .Create Lambda function for your backend
+    const backendFunction = new lambda.Function(this, 'BackendFunction', {
+      functionName: `${props.envName}-forkalicious-backend`,
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'lambda.handler',
+      code: lambda.Code.fromAsset(path.resolve(__dirname, '../../server/dist')),
+      environment: {
+        JWT_SECRET_KEY: ssm.StringParameter.valueForStringParameter(this, '/forkalicious/jwt-secret'),
+        SPOONACULAR_API_KEY: ssm.StringParameter.valueForStringParameter(this, '/forkalicious/spoonacular-api-key'),
+        API_BASE_URL: 'https://api.spoonacular.com',
+        OPENAI_API_KEY: ssm.StringParameter.valueForStringParameter(this, '/forkalicious/openai-api-key'),
+        MONGODB_URI: ssm.StringParameter.valueForStringParameter(this, '/forkalicious/mongodb-uri'),
+        NODE_ENV: props.envName === 'prod' ? 'production' : 'development'
       },
-    });*/
-    
-      // Attach OAC to the CloudFront Origin
-      // const cfnDistribution = distribution.node.defaultChild as cloudfront.CfnDistribution;
-      // cfnDistribution.addPropertyOverride("DistributionConfig.Origins.0.OriginAccessControlId", oac.ref);
-      // cfnDistribution.addPropertyOverride("DistributionConfig.Origins.0.S3OriginConfig.OriginAccessIdentity", "");
-  
-}
+      timeout: cdk.Duration.seconds(30),
+      memorySize: 256,
+      logRetention: logs.RetentionDays.ONE_WEEK
+    });
+
+    // Add permissions for SSM, CloudWatch, and basic execution
+    backendFunction.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        'ssm:GetParameter',
+        'ssm:GetParameters',
+        'ssm:GetParametersByPath',
+        'logs:CreateLogGroup',
+        'logs:CreateLogStream',
+        'logs:PutLogEvents'
+      ],
+      resources: [
+        `arn:aws:ssm:${this.region}:${this.account}:parameter/forkalicious/*`,
+        `arn:aws:logs:${this.region}:${this.account}:*`
+      ]
+    }));
+
+    // Create API Gateway logging role
+    const apiGatewayLoggingRole = new iam.Role(this, 'ApiGatewayLoggingRole', {
+      assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonAPIGatewayPushToCloudWatchLogs')
+      ]
+    });
+
+    // Create API Gateway account settings
+    const apiGatewayAccount = new apigateway.CfnAccount(this, 'ApiGatewayAccount', {
+      cloudWatchRoleArn: apiGatewayLoggingRole.roleArn
+    });
+
+    // Create API Gateway
+    const api = new apigateway.RestApi(this, `${props.envName}BackendApi`, {
+      restApiName: `${props.envName}BackendService`,
+      deploy: true,
+      deployOptions: {
+        stageName: 'prod',  // This is required by API Gateway
+        loggingLevel: apigateway.MethodLoggingLevel.INFO,
+        dataTraceEnabled: true
+      },
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+        allowHeaders: [
+          'Content-Type',
+          'X-Amz-Date',
+          'Authorization',
+          'X-Api-Key',
+          'X-Amz-Security-Token',
+          'X-Amz-User-Agent',
+          'Apollo-Require-Preflight',
+          'x-apollo-operation-name',
+          'x-apollo-operation-type'
+        ],
+        allowCredentials: true
+      }
+    });
+
+    // Add dependency to ensure account settings are created first
+    api.node.addDependency(apiGatewayAccount);
+
+    // Make sure the integration is properly set up
+    const backendIntegration = new apigateway.LambdaIntegration(backendFunction, {
+      proxy: true
+    });
+
+    // Add a test endpoint to verify basic connectivity
+    const testResource = api.root.addResource('test');
+    testResource.addMethod('GET', backendIntegration);
+
+    // Add GraphQL endpoint
+    const graphqlResource = api.root.addResource('graphql');
+    graphqlResource.addMethod('POST', backendIntegration);
+
+    // Add proxy integration for all other routes
+    api.root.addProxy({
+      defaultIntegration: backendIntegration,
+      anyMethod: true
+    });
+
+    new cdk.CfnOutput(this, 'ApiGatewayUrl', {
+      value: api.url,
+      description: 'API Gateway URL'
+    });
+  }
 }
