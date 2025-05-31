562618bccd04ea5b8788093bcef96548755dd7c4
Daniel <danstraws@gmail.com>
2025-05-31 08:42:43 -0400

    simplified the zip command

--- zip.sh
+++ zip.sh
@@ -1,1 +1,1 @@
-zip -r forktastic.zip . --exclude ".env" "./server/env" "node_modules/*" "./server/node_modules/*" "./server/dist/*" "./client/node_modules/*" "./client/dist/*"
+zip -r forktastic . --exclude "*.env" ".git" "*node_modules/*" "*/dist/*" "./zip.sh"
