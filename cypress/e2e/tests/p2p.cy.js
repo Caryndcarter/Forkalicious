97c1a5db35e2e00f9018c0c18b713dc150290396
Daniel <danstraws@gmail.com>
2025-06-02 02:09:46 -0400

    updated unit tests

--- cypress/e2e/tests/p2p.cy.js
+++ cypress/e2e/tests/p2p.cy.js
@@ -2,6 +2,45 @@ import credentials from "../testingAssets/accountCredentials";
 
 export function runP2PTests() {
   describe("The page works as it did before the edits", () => {
+
+    it("Logs in", () => {
+      // go to the account page:
+      cy.visit("/");
+      cy.get("#homepage-call-to-action").should('exist');
+      cy.get("#nav-account-link").click();
+
+      // login
+      cy.get("#userEmail").type(credentials.testingAccount.email);
+      cy.get("#userPassword").type(credentials.testingAccount.password);
+      cy.get("#sign-in-submit").click();
+
+      // check that it redirected to the home page
+      cy.get("#homepage-call-to-action").should('exist');
+
+    });
+
+    it("Can edit recipies", () => {
+
+      // go to the account page:
+      cy.visit("/");
+      cy.get("#homepage-call-to-action").should('exist');
+      cy.get("#nav-account-link").click();
+
+      // login
+      cy.get("#userEmail").type(credentials.testingAccount.email);
+      cy.get("#userPassword").type(credentials.testingAccount.password);
+      cy.get("#sign-in-submit").click();
+
+      // check that it redirected to the home page
+      cy.get("#homepage-call-to-action").should('exist');
+
+      // navigate to editing seciton
+      cy.get("#nav-recipe-book-link").click();
+      cy.get("#saved-recipes Button").click();
+      cy.get("#edit-button").click();
+
+    });
+
     it("sucessfully calls the AI generation route on editing page", () => {
 
       cy.intercept('POST', '**/open/ask/component', {
