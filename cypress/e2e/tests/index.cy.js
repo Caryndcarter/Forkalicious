3a9b937865cc41b0fc10a183ef8a5454f8c44f20
Daniel <danstraws@gmail.com>
2025-06-03 13:17:28 -0400

    updated tests

--- cypress/e2e/tests/index.cy.js
+++ cypress/e2e/tests/index.cy.js
@@ -1,9 +1,7 @@
 import { runP2PTests } from "./p2p.cy";
-import runMakerTests from "./maker-tests.cy";
-import runEditTests from "./edit-tests.cy";
+import { runUnitTests } from "./unitTests.cy";
 
 export default function runTests(){
     runP2PTests();
-    runMakerTests();
-    runEditTests();
+    runUnitTests();
 }
