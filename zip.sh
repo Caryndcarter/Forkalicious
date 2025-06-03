5b435e97c8dfa69b5e4110d472f1c36afda3ad98
Daniel <danstraws@gmail.com>
2025-06-03 13:11:23 -0400

    updated zip command for this branch

--- zip.sh
+++ zip.sh
@@ -1,1 +1,1 @@
-zip -r forktastic_DA_postedit . --exclude "*.env" ".git/*" "*node_modules/*" "*/dist/*" "./zip.sh" "run_my_docker.sh"
+zip -r forktastic_preedit . --exclude "*.env" ".git/*" "*node_modules/*" "*/dist/*" "./zip.sh" "run_my_docker.sh"
