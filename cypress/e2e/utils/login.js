016fe85d0a891a53e1c2ee8519a1032835f6ac00
Daniel <danstraws@gmail.com>
2025-06-03 20:06:16 -0400

    fixed issue with the login util and createRecipe. Added a unit test that checks each for overwritting the fields.

--- cypress/e2e/utils/login.js
+++ cypress/e2e/utils/login.js
@@ -8,7 +8,4 @@ export default function login(){
   cy.get("#userEmail").type(credentials.testingAccount.email);
   cy.get("#userPassword").type(credentials.testingAccount.password);
   cy.get("#sign-in-submit").click();
-
-  // check that it redirected to the home page
-  cy.get("#homepage-call-to-action").should('exist');
 }
