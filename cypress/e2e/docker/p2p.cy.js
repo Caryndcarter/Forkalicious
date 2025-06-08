f4cf015697eba38fc69b509ef6587d8fc6af8268
Daniel <danstraws@gmail.com>
2025-06-08 13:37:39 -0400

    moved expected response to testing assets form. added unit tests, and fixed p2p tests in docker tests.

--- cypress/e2e/docker/p2p.cy.js
+++ cypress/e2e/docker/p2p.cy.js
@@ -1,52 +1,13 @@
-export function runP2PTests() {
-  describe("The page works as it did before the edits", () => {
-    it("should retrieve information for Buttermilk Skillet Fried Chicken (ID 636574) from Spoonacular", () => {
-      const expectedResponse = {
-        title: "Buttermilk Skillet Fried Chicken",
-        summary:
-          'You can never have too many Southern recipes, so give Buttermilk Skillet Fried Chicken a try. One portion of this dish contains roughly <b>49g of protein</b>, <b>43g of fat</b>, and a total of <b>657 calories</b>. For <b>$1.36 per serving</b>, this recipe <b>covers 26%</b> of your daily requirements of vitamins and minerals. This recipe serves 4. 5 people found this recipe to be yummy and satisfying. It works well as a rather cheap main course. It is brought to you by Foodista. Head to the store and pick up egg, paprika, pepper, and a few other things to make it today. From preparation to the plate, this recipe takes about <b>45 minutes</b>. Overall, this recipe earns a <b>good spoonacular score of 66%</b>. If you like this recipe, take a look at these similar recipes: <a href="https://spoonacular.com/recipes/buttermilk-skillet-fried-chicken-1350573">Buttermilk Skillet Fried Chicken</a>, <a href="https://spoonacular.com/recipes/buttermilk-skillet-fried-chicken-1275131">Buttermilk Skillet Fried Chicken</a>, and <a href="https://spoonacular.com/recipes/favorite-buttermilk-skillet-fried-chicken-from-mastering-the-art-of-southern-cooking-149110">Favorite Buttermilk Skillet-Fried Chicken from \'Mastering the Art of Southern Cooking</a>.',
-        readyInMinutes: 45,
-        servings: 4,
-        ingredients: [
-          "2 cups buttermilk",
-          "3 pounds chicken pieces : drumsticks",
-          "1 tablespoon cornstarch",
-          "1  egg",
-          "1 tablespoon paprika",
-          "1 tablespoon pepper",
-          "0.25 c flour",
-          "0.5 teaspoon salt",
-          "4 servings vegetable oil",
-        ],
-        instructions:
-          "<ol><li>Clean all the chicken pieces well, remove skin if desired. Using 1 tablespoon salt and  tablespoon pepper, season the chicken well all over.</li><li>In a medium bowl whisk the buttermilk, egg and  cup of water.</li><li>In a large glass baking pan mix together: flour,1 tablespoon salt,  tablespoon pepper, paprika and cornstarch. Mix well.</li><li>Dip chicken in buttermilk then on flour mixture.  shaking off any excess flour.</li><li>In a large heavy bottom skillet heat the oil to a depth of . When oil is hot but not burning add the chicken to the skillet making sure that it is not crowding the pan. Fry chicken turning with thongs so it brown evenly. About 10 to 13 minutes.</li><li>When the chicken is done place on a paper plate and serve immediately.</li></ol>",
-        steps: [
-          "<ol><li>Clean all the chicken pieces well, remove skin if desired",
-          " Using 1 tablespoon salt and  tablespoon pepper, season the chicken well all over",
-          "</li><li>In a medium bowl whisk the buttermilk, egg and  cup of water",
-          "</li><li>In a large glass baking pan mix together: flour,1 tablespoon salt,  tablespoon pepper, paprika and cornstarch",
-          " Mix well",
-          "</li><li>Dip chicken in buttermilk then on flour mixture",
-          "  shaking off any excess flour",
-          "</li><li>In a large heavy bottom skillet heat the oil to a depth of ",
-          " When oil is hot but not burning add the chicken to the skillet making sure that it is not crowding the pan",
-          " Fry chicken turning with thongs so it brown evenly",
-          " About 10 to 13 minutes",
-          "</li><li>When the chicken is done place on a paper plate and serve immediately",
-          "</li></ol>",
-        ],
-        diet: [],
-        image: "https://img.spoonacular.com/recipes/636574-556x370.jpg",
-        sourceUrl:
-          "https://www.foodista.com/recipe/4T7XHVTV/buttermild-skillet-fried-chicken",
-        spoonacularSourceUrl:
-          "https://spoonacular.com/buttermilk-skillet-fried-chicken-636574",
-        spoonacularId: 636574,
-      };
+import { exampleRecipeResponse } from "../testingAssets/recipeExample";
 
-      cy.request("GET", "/open/information/636574").then((response) => {
+export function runP2PTests() {
+  describe("The page works as well as it did before the edits", () => {
+    it("should retrieve information for Buttermilk Skillet Fried Chicken (ID 636574) from Spoonacular", () => {
+      const getURL = `/open/information/${exampleRecipeResponse.id}`;
+
+      cy.request("GET", getURL).then((response) => {
         expect(response.status).to.eq(200);
-        expect(response.body).to.deep.equal(expectedResponse);
+        expect(response.body).to.deep.equal(exampleRecipeResponse.recipe);
       });
     });
 
@@ -63,7 +24,71 @@ export function runP2PTests() {
       });
     });
 
-    it('should confirm the first item for "chicken" query', () => {
+    it("should retrieve information from Edamam", () => {
+      const endpoint = "/open/information/0f8a23d14847480ceff80d20ae05101e";
+
+      cy.request("GET", endpoint).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property("title");
+        expect(response.body.title).to.eq(
+          "Mint Chocolate Shortbread Snowflakes"
+        );
+      });
+    });
+
+    it("should retrieve specific recipies from Spoonacular and Edamam", () => {
+      const endpoint = "/open/recipes";
+
+      const requestBody = {
+        query: "chicken",
+      };
+
+      const spoonacularRegex = /^\d+$/;
+      const edamamRegex = /^[0-9a-fA-F]{32}$/;
+
+      cy.request("POST", endpoint, requestBody).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.be.an("array");
+        expect(response.body).to.have.length.of.at.least(2);
+
+        cy.log(JSON.stringify(response.body[0]));
+        cy.log(JSON.stringify(response.body[1]));
+
+        const firstItem = response.body[0];
+        expect(firstItem).to.have.property("spoonacularId");
+        expect(firstItem.spoonacularId).to.match(spoonacularRegex);
+        expect(firstItem).to.have.property("title");
+        expect(firstItem.title).to.eq("Boozy Bbq Chicken");
+
+        const secondItem = response.body[1];
+        expect(secondItem).to.have.property("spoonacularId");
+        expect(secondItem.spoonacularId).to.match(edamamRegex);
+        expect(secondItem).to.have.property("title");
+        expect(secondItem.title).to.eq("Chicken Chashu");
+      });
+    });
+
+    it("should randomly retrieve from both Spoonacular and Edamam", () => {
+      const endpoint = "/open/random/";
+      const spoonacularRegex = /^\d+$/;
+      const edamamRegex = /^[0-9a-fA-F]{32}$/;
+
+      cy.request("GET", endpoint).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.be.an("array");
+        expect(response.body).to.have.length.of.at.least(2);
+
+        const firstItem = response.body[0];
+        expect(firstItem).to.have.property("spoonacularId");
+        expect(firstItem.spoonacularId).to.match(spoonacularRegex);
+
+        const secondItem = response.body[1];
+        expect(secondItem).to.have.property("spoonacularId");
+        expect(secondItem.spoonacularId).to.match(edamamRegex);
+      });
+    });
+
+    it('should confirm the "chicken" query takes both spoonacular and edaman', () => {
       const requestBody = {
         query: "chicken",
       };
@@ -74,24 +99,22 @@ export function runP2PTests() {
         title: "Boozy Bbq Chicken",
       };
 
-      cy.request("POST", "/open/recipes", requestBody).then((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body).to.be.an("array");
-        expect(response.body).to.not.be.empty;
-        expect(response.body[0]).to.deep.equal(expectedFirstItem);
-      });
-    });
-
-    it("should confirm search results give 9 recipes", () => {
-      const requestBody = {
-        query: "chicken",
+      const expectedSecondItem = {
+        spoonacularId: "fac0fed123103b648c8d6c46353cf8a5",
+        title: "Chicken Chashu",
       };
 
       cy.request("POST", "/open/recipes", requestBody).then((response) => {
         expect(response.status).to.eq(200);
         expect(response.body).to.be.an("array");
         expect(response.body).to.not.be.empty;
-        expect(response.body).to.have.lengthOf(9);
+        expect(response.body[0]).to.deep.equal(expectedFirstItem);
+
+        expect(response.body[1].spoonacularId).to.equal(
+          expectedSecondItem.spoonacularId
+        );
+
+        expect(response.body[1].title).to.equal(expectedSecondItem.title);
       });
     });
   });
